9172da414f03d25785978ef175c3521ef2400d51
Wictor Ducla <wictor.ducla14@gmail.com>
2024-02-21 08:52:17 -0300

    implementando o menu na parte de escolha da marca do veiculo e retornando todos os modelos disponiveis

---
+++ src/main/java/br/com/wictorducla/TabelaFipe/model/Dados.java
@@ -0,0 +1,4 @@
+package br.com.wictorducla.TabelaFipe.model;
+
+public record Dados(String codigo, String nome) {
+}
--- src/main/java/br/com/wictorducla/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/wictorducla/TabelaFipe/principal/Principal.java
@@ -1,12 +1,17 @@
 package br.com.wictorducla.TabelaFipe.principal;
 
+import br.com.wictorducla.TabelaFipe.model.Dados;
+import br.com.wictorducla.TabelaFipe.model.Modelos;
 import br.com.wictorducla.TabelaFipe.service.ConsumoApi;
+import br.com.wictorducla.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
     public void exibeMenu(){
         var menu = """
@@ -31,5 +36,23 @@ public class Principal {
 
         var json = consumo.obterDados(endereco);
         System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o codigo da marca para consultar: ");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
     }
 }
