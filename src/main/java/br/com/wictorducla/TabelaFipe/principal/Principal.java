363578f0faa68316dd6d3f2d47051f29c5107dd5
Wictor Ducla <wictor.ducla14@gmail.com>
2024-02-21 09:22:38 -0300

    Desafio completo, na ultima etapa implementei a busca final do veiculo dando uma opção por escrever o nome e fazer um filto para poder escolher o codigo do veiculo e para melhorar a busca ele já mostra todos os anos disponiveis do veiculo desejado

--- src/main/java/br/com/wictorducla/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/wictorducla/TabelaFipe/principal/Principal.java
@@ -2,11 +2,15 @@ package br.com.wictorducla.TabelaFipe.principal;
 
 import br.com.wictorducla.TabelaFipe.model.Dados;
 import br.com.wictorducla.TabelaFipe.model.Modelos;
+import br.com.wictorducla.TabelaFipe.model.Veiculo;
 import br.com.wictorducla.TabelaFipe.service.ConsumoApi;
 import br.com.wictorducla.TabelaFipe.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -54,5 +58,34 @@ public class Principal {
         modeloLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        System.out.println("Digite um trecho do nome do veiculo desejado: ");
+        var nomeVeiculo = leitura.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("Modelos filtrados: ");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("Digite o codigo do modelo desejado: ");
+        var codigoModelo = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumo.obterDados(endereco);
+
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i =0; i<anos.size(); i++){
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = consumo.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("Veiculo encontrado com todos os anos de fabricação: ");
+        veiculos.forEach(System.out::println);
     }
 }
